154a60069a67a4f4e329364dd268a4c946b46abb
Scottie Enriquez <scott.c.enriquez@gmail.com>
2019-10-06 19:16:57 -0500

    Adding skeleton

---
+++ Startup.cs
@@ -0,0 +1,61 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace visual_studio_live_2019_endpoint_routing_and_healthchecks
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+            services.AddHealthChecks();
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+                endpoints.Map("/startup", context =>
+                {
+                    return context.Response.WriteAsync("This is from Startup.cs");
+                });
+                endpoints.MapHealthChecks("/health");
+            });
+        }
+
+        private Task RequestDelegate(HttpContext context)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ WeatherForecast.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace visual_studio_live_2019_endpoint_routing_and_healthchecks
+{
+    public class WeatherForecast
+    {
+        public DateTime Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public string Summary { get; set; }
+    }
+}
